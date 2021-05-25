c12dfcb1e7994cf251e8ace28e6027ab18200afe
ruliii <raul.corrales.avila@gmail.com>
2021-05-25 18:50:59 +0200

    Update __main__.py

--- Skill de Alexa/add_producto/__main__.py
+++ Skill de Alexa/add_producto/__main__.py
@@ -1,14 +1,10 @@
 from cloudant.client import Cloudant
 from datetime import datetime, date
-client = Cloudant.iam(None, "VsaAFud5PEb_NTu-P6q2XTPKuPMdh2d3dBk4sjcBuczl",
-                      url= "https://apikey-v2-1ydxv3yyaitx75lhyqswq5lfpzx5tmgmouuh5phw8peh:44575386a643de8a12d5098f93a892ab@f1890360-b63a-49e2-bd2a-33704f0e8497-bluemix.cloudantnosqldb.appdomain.cloud",
-                      connect=True)
-
 
 def main(args):
     print("{0}--Estableciendo conexion con la base de datos".format(datetime.now()))
-    client = Cloudant.iam(None, "VsaAFud5PEb_NTu-P6q2XTPKuPMdh2d3dBk4sjcBuczl",
-                      url= "https://apikey-v2-1ydxv3yyaitx75lhyqswq5lfpzx5tmgmouuh5phw8peh:44575386a643de8a12d5098f93a892ab@f1890360-b63a-49e2-bd2a-33704f0e8497-bluemix.cloudantnosqldb.appdomain.cloud",
+    client = Cloudant.iam(None, "apiKey",
+                      url= "url",
                       connect=True)
     database = client['listascompra']
     document = database[args['id']]
